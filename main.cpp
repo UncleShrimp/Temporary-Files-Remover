b3a3af44212af4d3aff2916612f7aa22500f9075
Uncle._.Shrimp <133576545+UncleShrimp@users.noreply.github.com>
2025-10-24 01:28:23 +0300

    Deletion Update

--- main.cpp
+++ main.cpp
@@ -19,20 +19,24 @@ int main() {
     string str_directoryPath = "C:/Users/Uncle_Shrimp/AppData/Local/Temp";
     path directoryPath = str_directoryPath;
     for (const auto& entry :
-            directory_iterator(directoryPath)) {
-        // Output the path of the file or subdirectory
-        cout << "File: " << entry.path() << endl;
-        string path_string_u8 = entry.path().u8string();
-        cout<<formRemoveFilename(path_string_u8)<<endl<<endl;
-        string filename = formRemoveFilename(path_string_u8);
-        // Attempt to remove the file
-        int result = remove(filename);
+        directory_iterator(directoryPath)) {
+        
 
-        if (result == 0) {
-            std::cout << "File '" << filename << "' deleted successfully." << std::endl;
-        } else {
-            std::cerr << "Error deleting file '" << filename << "'. Error code: " << result << std::endl;
-            // You can use perror("Error message") to get a more descriptive error message
+
+        if (filesystem::is_regular_file(entry.path())) {
+            try {
+                // Output the path of the file or subdirectory
+                cout << "File: " << entry.path() << endl;
+                string path_string_u8 = entry.path().u8string();
+                cout<<formRemoveFilename(path_string_u8)<<endl<<endl;
+                string filename = formRemoveFilename(path_string_u8);
+                int result = remove(filename);
+                // Attempt to remove the file
+                filesystem::remove(filename);
+                std::cout << "Deleted: " << entry.path() << std::endl;
+            } catch (const filesystem::filesystem_error& e) {
+                std::cerr << "Error deleting " << entry.path() << ": " << e.what() << std::endl;
+            }
         }
     }
 }
